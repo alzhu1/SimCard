5e98bccda898a24dc572ed97233d7b2dcc2bfa5a
alzhu1 <er.and.alex@gmail.com>
2024-09-11 23:36:05 -0700

    Organize game assets into separate folders for card or simulation game

---
+++ Assets/SimGame/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimCard.SimGame {
+    public class Player : MonoBehaviour {
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+    }
+}
