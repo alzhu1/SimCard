b5dbdba7a14a08bdb6ecf2d78696f952c782e48e
alzhu1 <er.and.alex@gmail.com>
2025-02-16 16:25:29 -0800

    Add simple z-ordering

---
+++ Assets/Common/Scripts/SpriteSorter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimCard.Common {
+    public class SpriteSorter : MonoBehaviour {
+        [SerializeField] private float centerToBottomDistance = 1f;
+
+        private SpriteRenderer sr;
+
+        void Awake() {
+            sr = GetComponent<SpriteRenderer>();
+        }
+
+        void Update() {
+            sr.sortingOrder = Mathf.RoundToInt((transform.position.y - centerToBottomDistance) * 100f) * -1;
+        }
+    }
+}
