4619e4559ce9bbaa33133c7402366f57458457d4
alzhu1 <er.and.alex@gmail.com>
2025-03-02 17:59:07 -0800

    Add door opening/closing on entering trigger for buildings (updated for existing building prefabs)

---
+++ Assets/SimGame/Scripts/BuildingDoorTrigger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimCard.SimGame {
+    public class BuildingDoorTrigger : MonoBehaviour {
+        [SerializeField] private Sprite[] buildingSprites;
+        [SerializeField] private float frameTime = 0.25f;
+
+        private SpriteRenderer buildingSr;
+
+        void Awake() {
+            buildingSr = GetComponentInParent<SpriteRenderer>();
+        }
+
+        void OnTriggerEnter2D(Collider2D collider) {
+            if (collider.TryGetComponent(out Player player)) {
+                StartCoroutine(ChangeDoorState(true));
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D collider) {
+            if (collider.TryGetComponent(out Player player)) {
+                StartCoroutine(ChangeDoorState(false));
+            }
+        }
+
+        IEnumerator ChangeDoorState(bool toOpen) {
+            int index = toOpen ? 0 : buildingSprites.Length - 1;
+            int delta = toOpen ? 1 : -1;
+
+            while (index >= 0 && index < buildingSprites.Length) {
+                buildingSr.sprite = buildingSprites[index];
+                yield return new WaitForSeconds(frameTime);
+                index += delta;
+            }
+        }
+    }
+}
