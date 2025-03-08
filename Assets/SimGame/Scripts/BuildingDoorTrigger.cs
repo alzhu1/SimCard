81798b2b50c54a09bfc3428bf6399d39cfc34a65
alzhu1 <er.and.alex@gmail.com>
2025-03-08 12:23:27 -0800

    Add door closed states to buildings

--- Assets/SimGame/Scripts/BuildingDoorTrigger.cs
+++ Assets/SimGame/Scripts/BuildingDoorTrigger.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 namespace SimCard.SimGame {
     public class BuildingDoorTrigger : MonoBehaviour {
+        [SerializeField] private bool doorEnabled = true;
+        [SerializeField] private Collider2D openDoorParentCollider;
+        [SerializeField] private Collider2D closedDoorParentCollider;
+
         [SerializeField] private Sprite[] buildingSprites;
         [SerializeField] private float frameTime = 0.25f;
 
@@ -11,16 +15,18 @@ namespace SimCard.SimGame {
 
         void Awake() {
             buildingSr = GetComponentInParent<SpriteRenderer>();
+            openDoorParentCollider.enabled = doorEnabled;
+            closedDoorParentCollider.enabled = !doorEnabled;
         }
 
         void OnTriggerEnter2D(Collider2D collider) {
-            if (collider.TryGetComponent(out Player player)) {
+            if (doorEnabled && collider.TryGetComponent(out Player player)) {
                 StartCoroutine(ChangeDoorState(true));
             }
         }
 
         void OnTriggerExit2D(Collider2D collider) {
-            if (collider.TryGetComponent(out Player player)) {
+            if (doorEnabled && collider.TryGetComponent(out Player player)) {
                 StartCoroutine(ChangeDoorState(false));
             }
         }
